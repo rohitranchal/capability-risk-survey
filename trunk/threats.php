ddf6df211a5a2f1a5e0f7627d44e1c48451e955f
ruchith.fernando@gmail.com <ruchith.fernando@gmail.com@919f06a8-68f9-d3f4-c169-0bf7d4ed0dc1>
2013-06-16 22:21:27 +0000

    Moving the processing type parameter back to where it should be

--- trunk/threats.php
+++ trunk/threats.php
@@ -25,9 +25,9 @@ Systems</title>
 <p>Attackers may not always need to be security experts  who have all resources and time to perform successful attacks. For instance,  most attacks do not require deep security knowledge if appropriate tools are  available, e.g., Denial of Service (DoS) attack. In this form we assess the  likelihood of success of exercising the threats, which measures the chance that an attacker could exploit a weakness or  vulnerability in the system to trigger the threat. The success score combines,  equally, the scores for the factors: required security expertise, required  equipment and tools, knowledge of the system, elapsed time to find a weakness  and develop an attack, and window of opportunity to perform an attack (e.g.,  the system tolerates only 3 attempts of failed passwords). For each threat,  please select the level required of each factor to perform a successful attack.  
 <p>Note: The estimate is for the initial risk  assessment for a system to develop, you may consider the system does not have  security controls yet (Safeguards, features, and mechanisms that protect a  system from specific threats). 
 <p>
-  <input type="hidden" name="type" value="threats"/>
 
 <form action="process.php" method="post">
+  <input type="hidden" name="type" value="threats"/>
   <table>
 	<tr>
 		<th>Threat</th>
