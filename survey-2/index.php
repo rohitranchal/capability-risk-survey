b8af235e36d8ba728dba3b15cef46737e3a8a24c
ruchith.fernando <ruchith.fernando@gmail.com>
2013-11-10 20:26:01 +0000

    Adding images

--- survey-2/index.php
+++ survey-2/index.php
@@ -3,10 +3,10 @@ session_start();
 
 //Generate new session id
 if(isset($_SESSION['cap_id'])) {
-  //Fetch session
-  $id = $_SESSION['cap_id'];
+	//Fetch session
+	$id = $_SESSION['cap_id'];
 } else {
-  header("Location: ./pin.html");
+	header("Location: ./pin.html");
 }
 
 ?>
@@ -20,16 +20,27 @@ if(isset($_SESSION['cap_id'])) {
 <pre>Capability-based Security Risk Estimation and Mitigation for Information Systems</pre>
 <h2>Team members</h2>
 <p><a href="http://www.cs.purdue.edu/homes/bb">Prof. Bharat Bhargava</a><br>
-  <a href="http://www.win.tue.nl/~lothmane/">Dr. Lotfi Ben Othmane</a><br>
-  <a href="http://www.ruchith.org">Ruchith Fernando</a><br>
-  <a href="http://www.cs.purdue.edu/homes/rranchal">Rohit Ranchal</a></p>
+	<a href="http://www.win.tue.nl/~lothmane/">Dr. Lotfi Ben Othmane</a><br>
+	<a href="http://www.ruchith.org">Ruchith Fernando</a><br>
+	<a href="http://www.cs.purdue.edu/homes/rranchal">Rohit Ranchal</a></p>
 
 <h2> Project Introduction </h2>
 
 <p>The goal of this survey is to collect expert opinions about the likelihood of a set of threats to connected vehicles using two different approaches, which we describe later. You assume that the risk estimate is performed at the inception stage of the project; that is, technology choices, such as operating system and programming language, will be made in a later stage. You also consider that it is possible to trigger all the listed threats and the potential attacker does not need to be an expert. </p>
 <h2>Description of the System</h2>
 
-<p>A motor vehicle uses a set of sensors and Electronic Control Units (ECUs) to collect data about the vehicle's behavior and environment, and to control the functionalities of the vehicle. ECUs can collaborate by exchanging messages and form an in-vehicle network (a.k.a. on-Board network). Motor vehicles, until recently, used to have a closed in-vehicle network, i.e. they did not have external connectivity. Messages exchanged between the components of a vehicle were produced and consumed by the nodes of the in-vehicle network.</p>
+<table>
+<tr>
+	<td>A motor vehicle uses a set of sensors and Electronic Control Units (ECUs) to collect data about the vehicle's behavior and environment, and to control the functionalities of the vehicle. ECUs can collaborate by exchanging messages and form an in-vehicle network (a.k.a. on-Board network). Motor vehicles, until recently, used to have a closed in-vehicle network, i.e. they did not have external connectivity. Messages exchanged between the components of a vehicle were produced and consumed by the nodes of the in-vehicle network.<td>
+	<td><img width="500" src="in-Vehicle-ECU-Architecture.png"/></td>
+</tr>
+</table>
+
+
+<table>
+<tr>
+	<td>
+
 <p>Today,
 several applications such as cooperative adaptive cruise control, remote
 firmware update, e-call, and remote diagnostic of vehicles require
@@ -40,10 +51,12 @@ devices, and Service Centers (SCs). The assets of the connected vehicles
 include the occupants of the vehicle, the in-vehicle data and the
 functionalities of the vehicle components. The following video shows possible
 attack scenarios on a vehicle:</p>
+		<iframe width="420" height="315" src="//www.youtube.com/embed/0VZgU9ac_QI" frameborder="0" allowfullscreen></iframe>
+	</td>
+	<td><img width="300" src="Remotevehicle.jpeg"/></td>
+</tr>
+</table>	
 
-<div style="width: 50%; margin: 0 auto;">
-<iframe width="420" height="315" src="//www.youtube.com/embed/0VZgU9ac_QI" frameborder="0" allowfullscreen></iframe>
-</div>
 
 <h2>Survey</h2>
 
@@ -53,14 +66,14 @@ vehicle or prevent the systems of the vehicle from working properly. We
 appreciate that you provide us with your expert opinion about the likelihood of
 the threats that the attackers may want to cause. So, Please</p>
 <ol>
-  <li>Click <a href="threats.php">here</a> to provide your
+	<li>Click <a href="threats.php">here</a> to provide your
 opinions about the likelihood of the threats to the system by selecting
 perceived scores for the given factors that describe the likelihood of
 exercising the threats. </li>
-  <li>Click <a href="cap.php">here</a> to provide your
+	<li>Click <a href="cap.php">here</a> to provide your
 opinions about the attacker capabilities (e.g., attacker physical access to
 street camera) that an attacker may have or acquire.</li>
-  <li class="Capsurvey">Click<a href="threat_cap.php"> here</a> to provide
+	<li class="Capsurvey">Click<a href="threat_cap.php"> here</a> to provide
 your opinions about the likelihood of the threats to the system by selecting
 the perceived scores for the given factors that describe the likelihood of
 exercising the threat. Note that in this step, we provide the capabilities
@@ -70,7 +83,7 @@ related to each threat.</li>
 We thank you for accepting to participate in the survey and provide your expert opinion.</p>
 
 <p>All the information collected in this survey will be kept anonymous and will never be linked with the survey
-  takers. <br>
+	takers. <br>
 
 Questions and comments: <a href="mailto:bb-survey@cs.purdue.edu">bb-survey@cs.purdue.edu</a></p>
 
